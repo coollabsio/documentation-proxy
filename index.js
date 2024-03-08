c181b595b5b86f9d682f2881759790d99c785d74
Andras Bacsai <andras.bacsai@gmail.com>
2024-03-08 10:26:03 +0100

    Add preValidation logging in http-proxy registration

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const server = Fastify();
 
 server.register(require('@fastify/http-proxy'), {
     upstream: 'https://coolify.io/docs/',
+    preValidation: (req, reply, done) => {
+        console.log(req)
+        console.log('preValidation');
+        done();
+    },
     prefix: '/docs/',
 });
 server.register(require('@fastify/http-proxy'), {
