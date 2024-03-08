a93c5b760b5546dde6de27b220c42cfbd5211887
Andras Bacsai <andras.bacsai@gmail.com>
2024-03-08 10:30:10 +0100

    Update upstream URL and remove commented code

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const Fastify = require('fastify');
 const server = Fastify();
 console.log('hello')
 server.register(require('@fastify/http-proxy'), {
-    upstream: 'https://coolify.io/docs/',
+    upstream: 'https://coolify.io/docs',
     preValidation: (req, reply, done) => {
         console.log(req);
         done();
     },
-    prefix: '/docs/',
+    prefix: '/docs',
 });
-server.register(require('@fastify/http-proxy'), {
-    upstream: 'https://coolify.io/',
-    prefix: '/',
-});
+// server.register(require('@fastify/http-proxy'), {
+//     upstream: 'https://coolify.io/',
+//     prefix: '/',
+// });
 
 server.listen({ port: 3000 });
