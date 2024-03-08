fae4d5e4953e82ae7cd3974cef4416d5a4c6c888
Andras Bacsai <andras.bacsai@gmail.com>
2024-03-08 10:57:57 +0100

    Update prefix in http-proxy configuration

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const Fastify = require('fastify');
 const server = Fastify();
+// server.register(require('@fastify/http-proxy'), {
+//     upstream: 'https://docs.coolify.io/',
+//     prefix: '/docs', // Set the prefix to '/docs'
+//     rewritePrefix: '', // Ensure the path isn't rewritten
+// });
 server.register(require('@fastify/http-proxy'), {
     upstream: 'https://docs.coolify.io/',
-    prefix: '/docs', // Set the prefix to '/docs'
+    prefix: '/', // Set the prefix to '/docs'
     rewritePrefix: '', // Ensure the path isn't rewritten
 });
 
