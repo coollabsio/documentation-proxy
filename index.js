4e0dc9dffa8f93fa0bcdfda17e1f73125215292f
Andras Bacsai <andras.bacsai@gmail.com>
2024-03-08 10:29:11 +0100

    Add console.log statement in index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const Fastify = require('fastify');
 const server = Fastify();
-
+console.log('hello')
 server.register(require('@fastify/http-proxy'), {
     upstream: 'https://coolify.io/docs/',
     preValidation: (req, reply, done) => {
-        console.log(req)
-        console.log('preValidation');
+        console.log(req);
         done();
     },
     prefix: '/docs/',
