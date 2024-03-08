eeeaee6f74ed74a0e5404941a76695af2c334050
Andras Bacsai <andras.bacsai@gmail.com>
2024-03-08 10:48:20 +0100

    Remove preValidation console.log in index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const server = Fastify();
 console.log('hello')
 server.register(require('@fastify/http-proxy'), {
     upstream: 'https://coolify.io/docs',
-    preValidation: (req, reply, done) => {
-        console.log(req);
-        done();
-    },
     prefix: '/docs',
 });
 server.register(require('@fastify/http-proxy'), {
